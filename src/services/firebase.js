d98bd76c82185728ec44b97109082c22246425a8
Nelly Rozova <igorevna.nailya@mail.ru>
2023-01-09 02:18:59 +0300

    finish

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
 } from 'firebase/auth';
 
 import { getDatabase, ref } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAy_0BNttYBniEazZZIQLsb_5xABzr6FoY',
-    authDomain: 'gb-chats-c5c37.firebaseapp.com',
-    projectId: 'gb-chats-c5c37',
-    storageBucket: 'gb-chats-c5c37.appspot.com',
-    messagingSenderId: '490107659528',
-    appId: '1:490107659528:web:5c2d3f78e842d441a138bd',
+  apiKey: 'AIzaSyAy_0BNttYBniEazZZIQLsb_5xABzr6FoY',
+  authDomain: 'gb-chats-c5c37.firebaseapp.com',
+  projectId: 'gb-chats-c5c37',
+  storageBucket: 'gb-chats-c5c37.appspot.com',
+  messagingSenderId: '490107659528',
+  appId: '1:490107659528:web:5c2d3f78e842d441a138bd',
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,11 +22,11 @@ const app = initializeApp(firebaseConfig);
 export const firbaseAuth = getAuth(app);
 
 export const signUp = async (email, password) => {
-    await createUserWithEmailAndPassword(firbaseAuth, email, password);
+  await createUserWithEmailAndPassword(firbaseAuth, email, password);
 };
 
 export const signIn = async (email, password) => {
-    await signInWithEmailAndPassword(firbaseAuth, email, password);
+  await signInWithEmailAndPassword(firbaseAuth, email, password);
 };
 
 export const logOut = async () => await signOut(firbaseAuth);
